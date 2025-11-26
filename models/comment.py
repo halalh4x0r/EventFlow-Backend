b9c4c8099b87f08e48e0a35aee6f6396a9d63bb2
halalh4x0r <mohamed.hadi@student.moringaschool.com>
2025-11-26 14:57:44 +0300

    updated comment.py

--- models/comment.py
+++ models/comment.py
@@ -4,10 +4,9 @@ class Comment(db.Model):
     __tablename__ = "comments"
 
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String, nullable=False)
-
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    event_id = db.Column(db.Integer, db.ForeignKey("events.id"))
+    content = db.Column(db.String, nullable=False)  # this is required
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey("events.id"), nullable=False)
 
     # Relationships
     user = db.relationship("User", back_populates="comments")
@@ -16,7 +15,7 @@ class Comment(db.Model):
     def to_dict(self):
         return {
             "id": self.id,
-            "text": self.text,
+            "content": self.content,
             "user_id": self.user_id,
             "event_id": self.event_id
         }
