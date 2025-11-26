76cb1c8faa44cb6b1e5000b8ea5c51b790a7c462
halalh4x0r <mohamed.hadi@student.moringaschool.com>
2025-11-26 14:12:22 +0300

    modified event.py

--- models/event.py
+++ models/event.py
@@ -1,5 +1,27 @@
-from config import db
+from . import db
 
 class Event(db.Model):
     __tablename__ = "events"
+
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+    location = db.Column(db.String)
+    date = db.Column(db.String, nullable=False)
+
+    organizer_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    # Relationships
+    organizer = db.relationship("User", back_populates="events")
+    rsvps = db.relationship("RSVP", back_populates="event", cascade="all, delete-orphan")
+    comments = db.relationship("Comment", back_populates="event", cascade="all, delete-orphan")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "location": self.location,
+            "date": self.date,
+            "organizer_id": self.organizer_id
+        }
