b358405a3e61877021ec501e8beff3f16b9de549
halalh4x0r <mohamed.hadi@student.moringaschool.com>
2025-11-26 14:58:15 +0300

    updated rsvp.py

--- models/rsvp.py
+++ models/rsvp.py
@@ -4,10 +4,9 @@ class RSVP(db.Model):
     __tablename__ = "rsvps"
 
     id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.String, nullable=False)  # going, not-going, interested
-
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    event_id = db.Column(db.Integer, db.ForeignKey("events.id"))
+    status = db.Column(db.String, nullable=False, default="pending")  # give a default
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey("events.id"), nullable=False)
 
     # Relationships
     user = db.relationship("User", back_populates="rsvps")
