93c8eec61b5744155ccf496689326b6a97938f9f
halalh4x0r <mohamed.hadi@student.moringaschool.com>
2025-11-26 14:12:03 +0300

    modified comment.py

--- models/comment.py
+++ models/comment.py
@@ -1,5 +1,22 @@
-from config import db
+from . import db
 
 class Comment(db.Model):
     __tablename__ = "comments"
+
     id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    event_id = db.Column(db.Integer, db.ForeignKey("events.id"))
+
+    # Relationships
+    user = db.relationship("User", back_populates="comments")
+    event = db.relationship("Event", back_populates="comments")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "text": self.text,
+            "user_id": self.user_id,
+            "event_id": self.event_id
+        }
