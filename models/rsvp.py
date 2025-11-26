1b275555245679987bc3a2920e05a240effbc51c
halalh4x0r <mohamed.hadi@student.moringaschool.com>
2025-11-26 14:12:39 +0300

    modified rsvp.py

--- models/rsvp.py
+++ models/rsvp.py
@@ -1,5 +1,22 @@
-from config import db
+from . import db
 
 class RSVP(db.Model):
     __tablename__ = "rsvps"
+
     id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String, nullable=False)  # going, not-going, interested
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    event_id = db.Column(db.Integer, db.ForeignKey("events.id"))
+
+    # Relationships
+    user = db.relationship("User", back_populates="rsvps")
+    event = db.relationship("Event", back_populates="rsvps")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "status": self.status,
+            "user_id": self.user_id,
+            "event_id": self.event_id
+        }
